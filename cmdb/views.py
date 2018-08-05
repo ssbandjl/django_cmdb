f3830240566b349943e12eb0af14e57168779849
XB <ssbandjl@163.com>
2018-08-05 21:28:55 +0800

    增加index

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to XB's HomePage! More about in http:www.logread.cn")
+    return HttpResponse("Welcome to XB's HomePage!\nMore about in http:www.logread.cn")
