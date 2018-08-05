0ced29b5f9fae4c61015fd613c3741a61e784520
XB <ssbandjl@163.com>
2018-08-05 21:26:24 +0800

    增加index

--- cmdb/urls.py
+++ cmdb/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.conf.urls import include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^/', views.index),
