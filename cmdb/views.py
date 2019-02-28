ff494e29e761265aa98fc71cc2ec52ea9a561e15
songxiaobing <ssbandjl@163.com>
2019-02-28 16:38:26 +0800

    增加审批资产的相关注释

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Welcome to XB's HomePage!\nMore about in http:www.logread.cn")
+    # return HttpResponse("Welcome to XB's HomePage!\nMore about in http:www.logread.cn")
+    return render(request, 'index.html', locals())
